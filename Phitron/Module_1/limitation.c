bab956c5fbfb44a7a14839d4e1ffb273fc710ba5
Shuvo Paul <93747830+ShuvoPaul1@users.noreply.github.com>
2023-10-07 15:04:57 +0600

    Delete Phitron directory

--- Phitron/Module_1/limitation.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-long long int a=1000345343432423;
-printf("%lld",a);
-
-}
--- Phitron/Module_1/variable2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-int a=200,b=300;
-printf("%d\n%d",a,b);
-
-
-}
